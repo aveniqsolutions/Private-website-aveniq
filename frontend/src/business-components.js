2b58033afcac5278ad373cfdfad04a7fd9580e6b
aveniqsolutions <aveniqsolutions@gmail.com>
2026-03-05 00:27:37 +0100

    Update business-components.js

--- frontend/src/business-components.js
+++ frontend/src/business-components.js
@@ -1055,7 +1055,7 @@ export function Footer() {
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
           <p className="text-gray-400">
-            © 2024 Aveniq Solutions. All rights reserved. Built with modern technology.
+            © 2026 Aveniq Solutions. All rights reserved.
           </p>
         </div>
       </div>
